08b7f15dfd8127dbd66f907abe691aed189fab7e
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 07:56:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Syötä ikäsi: ");
+        int ikä = int.Parse(Console.ReadLine());
+
+        if (ikä < 0)
+        {
+            Console.WriteLine("Syötä positiivinen ikä.");
+        }
+        else if (ikä < 18)
+        {
+            Console.WriteLine($"{ikä} - näytät ikäistäsi nuoremmalta");
+        }
+        else
+        {
+            Console.WriteLine($"{ikä} - näytät ikäistäsi vanhemmalta");
+        }
+    }
+}
